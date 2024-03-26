532f10182485420aadf77715b0abf56ccb3b760d
henqix <40019931+henqix@users.noreply.github.com>
2024-03-26 11:15:23 +0100

    Update adv_queue.sql

--- adv_queue.sql
+++ adv_queue.sql
@@ -7,7 +7,7 @@ rem * Description       : Show message status for Advanced Queueing
 rem * Usage             : start adv_queue.sql
 rem ********************************************************************
 
-set lined 132
+set lines 132
 set pages 60
 
 column owner format a30
