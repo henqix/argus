c60495e0e5481609d4365b02ef068a6fdc0f402a
henqix <40019931+henqix@users.noreply.github.com>
2024-03-27 15:39:45 +0100

    Add files via upload

--- act_latch.sql
+++ act_latch.sql
@@ -1,29 +1,26 @@
-rem ********************************************************************
-rem * Filename          : act_latch.sql - Version 1.0
-rem * Author            : Henk Uiterwijk
-rem * Original          : IXORA
-rem * Last Update       : 27-sep-2001 
-rem * Update            : Adapted for Argus Toolbox
-rem * Description       : Report actual latches
-rem * Usage             : start act_latch.sql
-rem ********************************************************************
-
-set lines 128
-set pages 60
+-------------------------------------------------------------------------------
+--
+-- Script:	latch_where_now.sql
+-- Purpose:	shows a snapshot of latch sleeps by code locations
+-- For:		8.0 and higher
+--
+-- Copyright:	(c) Ixora Pty Ltd
+-- Author:	Steve Adams
+--
+-------------------------------------------------------------------------------
 
 set recsep off
+column name format a30 heading "LATCH TYPE"
+column location format a40 heading "CODE LOCATION and [LABEL]"
+column sleeps format 999999 heading "SLEEPS"
 
-column name format a30 heading "Latch type"
-column location format a40 heading "Code location and [Label]"
-column sleeps format 999999 heading "Sleeps"
-
-select
+select /*+ ordered use_merge(b) */
   b.name,
   b.location,
   b.sleeps - a.sleeps  sleeps
 from
   (
-    select
+    select /*+ no_merge */
       wsc.ksllasnam  name,
       rpad(lw.ksllwnam, 40) ||
       decode(lw.ksllwlbl, null, null, '[' || lw.ksllwlbl || ']')  location,
@@ -34,30 +31,28 @@ from
     where
       wsc.inst_id = userenv('Instance') and
       lw.inst_id = userenv('Instance') and
-      wsc.kslsleep > 0 and
       lw.indx = wsc.indx
   )  a,
   (
-    select
+    select /*+ no_merge */
       wsc.ksllasnam  name,
       rpad(lw.ksllwnam, 40) ||
       decode(lw.ksllwlbl, null, null, '[' || lw.ksllwlbl || ']')  location,
       wsc.kslsleep  sleeps
     from
+      ( select min(indx) zero from sys.x$ksmmem where rownum < 1000000 ) delay,
       sys.x$kslwsc wsc,
       sys.x$ksllw lw
     where
       wsc.inst_id = userenv('Instance') and
       lw.inst_id = userenv('Instance') and
-      wsc.kslsleep > 0 and
+      wsc.kslsleep > delay.zero and
       lw.indx = wsc.indx
   )  b
 where
-  b.name = a.name (+) and
-  b.location = a.location (+) and
+  b.name = a.name and
+  b.location = a.location and
   b.sleeps > a.sleeps
 order by
   3 desc
 /
-
-set lines 80
