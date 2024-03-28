532e0c603a5e9c6fac46327aa2566da251deecd1
henqix <40019931+henqix@users.noreply.github.com>
2024-03-28 08:18:37 +0100

    Add files via upload

---
+++ dsk_writes.sql
@@ -0,0 +1,48 @@
+rem ********************************************************************
+rem * Filename          : dsk_writes.sql - Version 2.0
+rem * Author            : Henk Uiterwijk
+rem * Original          : 03-OCT-99
+rem * Last Update       : 03-JAN-02
+rem * Update            : Value for DISK WRITES added.
+rem * Description       : Monitor database big disk writers
+rem * Usage             : start dsk_writes.sql
+rem ********************************************************************
+
+set lines 255
+set pages 60
+
+column executes format 9,999,999
+column dwrites  format 999,999,999,999 heading "DISK WRITES"
+column load     format a10
+column address  format a16
+
+break on load on executes on dwrites on address on sql_id on plan_hash_value 
+
+select
+  substr(
+    to_char(
+      100 * s.DIRECT_WRITES / t.total_disk_writes,
+      '99.00'
+    ),
+    2
+  ) ||
+  '%'  load,
+  s.DIRECT_WRITES dwrites,
+  s.executions executes,
+  p.sql_text,
+  s.address,
+  s.sql_id,
+  s.plan_hash_value
+from
+  ( select sum(direct_writes) total_disk_writes from sys.v_$sql )  t,
+  sys.v_$sql  s,
+  sys.v_$sqltext  p
+where
+  100 * s.direct_writes / t.total_disk_writes > 1 and
+  s.direct_writes > 50 * s.executions and
+  p.address = s.address
+order by
+  1, s.address, p.piece
+/
+
+start rf_clea
