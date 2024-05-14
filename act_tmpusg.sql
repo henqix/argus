d822e0d7c67c943f6756b1f5606ed7b3f95dc7b7
henqix <40019931+henqix@users.noreply.github.com>
2024-05-14 10:37:53 +0200

    Add files via upload

--- act_tmpusg.sql
+++ act_tmpusg.sql
@@ -21,9 +21,15 @@ select substr(TABLESPACE_NAME,1,10) "TABLESPACE",
 from v$sort_segment
 /
 
-select s.sid, s.serial#, s.username, s.osuser, u.extents, u.blocks, u.sqladdr, u.segrfno#
+column username    format a15
+column osuser      format a20
+column ext         format 999
+column blck        format 999
+
+select s.sid, s.serial#, s.username, s.osuser, u.extents ext, u.blocks blck, u.sqladdr, u.segrfno#
 from   v$session s,
        v$sort_usage u
 where  s.SADDR = u.SESSION_ADDR
+order by u.extents desc
 /
 
